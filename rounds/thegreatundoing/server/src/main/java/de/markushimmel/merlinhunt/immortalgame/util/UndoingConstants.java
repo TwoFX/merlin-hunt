57f311ed02d64623c0b222ca0ac52e2184c06671
Markus Himmel <markus@himmel-villmar.de>
2022-11-28 13:49:33 +0100

    [greatundoing] change size to 101

--- rounds/thegreatundoing/server/src/main/java/de/markushimmel/merlinhunt/immortalgame/util/UndoingConstants.java
+++ rounds/thegreatundoing/server/src/main/java/de/markushimmel/merlinhunt/immortalgame/util/UndoingConstants.java
@@ -3,6 +3,6 @@ package de.markushimmel.merlinhunt.immortalgame.util;
 public interface UndoingConstants {
 
     // Must be odd
-    static final int SIZE = 5;
+    static final int SIZE = 101;
 
 }
