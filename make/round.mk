51b4a13e0f8954cf3abf7d19d9e727d808b3d1d2
Markus Himmel <markus@himmel-villmar.de>
2022-12-24 12:00:08 +0100

    Get started on inputs

--- make/round.mk
+++ make/round.mk
@@ -2,8 +2,10 @@
 SUFFIX := This is a secret suffix
 
 flag: $(FLAGNAME).flag
+	cp $< ../target/flags
 
 dj-zip: $(SHORTNAME).zip
+	cp $< ../target/domjudge
 
 $(FLAGNAME).flag:
 	printf "$(PROBLEMNAME)$(SUFFIX)" | sha256sum > $@
@@ -27,4 +29,10 @@ $(SHORTNAME).zip: dj-contest/domjudge-problem.ini dj-contest/data/secret/1.in dj
 	(cd dj-contest && zip ../$@ `printf "$^" | sed "s/dj-contest\\///g" -`)
 
 clean::
-	rm -rf dj-contest $(FLAGNAME).flag $(SHORTNAME).zip
+	rm -rf dj-contest $(FLAGNAME).flag $(SHORTNAME).zip $(FLAGNAME).zip
+
+zip: $(FLAGNAME).zip
+	cp $< ../target/problems
+
+$(FLAGNAME).zip: $(INPUTFILES)
+	(cd input && zip ../$@ $(INPUTFILES))
