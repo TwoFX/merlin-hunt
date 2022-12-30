97d90a1e4b8e44ae2bf191030c6856c5f341773c
Markus Himmel <markus@himmel-villmar.de>
2022-12-30 18:14:50 +0100

    [twistylittlepackages] server configuration

--- deploy/up.sh
+++ deploy/up.sh
@@ -2,10 +2,12 @@
 
 sudo ufw-docker delete allow registry
 sudo ufw-docker delete allow nginx
+sudo ufw-docker delete allow twisty
 
 docker compose up -d
 
 sudo ufw-docker allow registry 5000
 sudo ufw-docker allow nginx 80
 sudo ufw-docker allow nginx 443
+sudo ufw-docker allow twisty 5432
 sudo ufw reload
