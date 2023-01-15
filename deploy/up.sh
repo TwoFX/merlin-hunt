3a5acbece0204e98ea805a787936a98d6aaf2dfa
Markus Himmel <markus@himmel-villmar.de>
2023-01-15 17:15:13 +0100

    GitLab

--- deploy/up.sh
+++ deploy/up.sh
@@ -2,10 +2,12 @@
 
 sudo ufw-docker delete allow nginx
 sudo ufw-docker delete allow twisty
+sudo ufw-docker delete allow gitlab
 
 docker compose up -d
 
 sudo ufw-docker allow nginx 80
 sudo ufw-docker allow nginx 443
 sudo ufw-docker allow twisty 5432
+sudo ufw-docker allow gitlab 22
 sudo ufw reload
