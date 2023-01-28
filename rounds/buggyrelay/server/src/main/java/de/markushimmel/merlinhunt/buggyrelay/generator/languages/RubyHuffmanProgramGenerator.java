4330f1bab62d58f957ebeceb9b4877f3ecfffccd
Markus Himmel <markus@himmel-villmar.de>
2023-01-28 17:56:30 +0100

    [buggyrelay] get started on Huffman

--- rounds/buggyrelay/server/src/main/java/de/markushimmel/merlinhunt/buggyrelay/generator/languages/RubyHuffmanProgramGenerator.java
+++ rounds/buggyrelay/server/src/main/java/de/markushimmel/merlinhunt/buggyrelay/generator/languages/RubyHuffmanProgramGenerator.java
@@ -1,5 +1,8 @@
 package de.markushimmel.merlinhunt.buggyrelay.generator.languages;
 
+import java.util.Map;
+import java.util.PriorityQueue;
+
 import de.markushimmel.merlinhunt.buggyrelay.generator.IProgramGenerator;
 
 public class RubyHuffmanProgramGenerator implements IProgramGenerator {
@@ -10,4 +13,35 @@ public class RubyHuffmanProgramGenerator implements IProgramGenerator {
         return null;
     }
 
+    private HuffmanNode createHuffmanTree(Map<Character, Integer> frequencies) {
+        PriorityQueue<HuffmanNode> queue = new PriorityQueue<>();
+
+        for (var entry : frequencies.entrySet()) {
+            queue.add(new HuffmanLeaf(entry.getValue(), entry.getKey()));
+        }
+
+        while (queue.size() >= 2) {
+            HuffmanNode left = queue.poll();
+            HuffmanNode right = queue.poll();
+            int newFrequency = left.frequency() + right.frequency();
+            queue.add(new HuffmanInnerNode(newFrequency, left, right));
+        }
+
+        return queue.poll();
+    }
+
+    private sealed interface HuffmanNode extends Comparable<HuffmanNode> permits HuffmanLeaf, HuffmanInnerNode {
+        int frequency();
+
+        default int compareTo(HuffmanNode other) {
+            return Integer.compare(frequency(), other.frequency());
+        }
+    }
+
+    private record HuffmanLeaf(int frequency, char character) implements HuffmanNode {
+    }
+
+    private record HuffmanInnerNode(int frequency, HuffmanNode leftSubtree, HuffmanNode rightSubtree)
+            implements HuffmanNode {
+    }
 }
